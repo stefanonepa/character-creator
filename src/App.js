fdbec6db956eea213e8e36cef84d2b8f8b7e3f28
stefanovualto <stefano.nepa@vualto.com>
2021-04-10 23:21:46 +0100

    add decoration

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import PerspectiveDecoration from './components/PerspectiveDecoration';
 
 function App() {
-  return (
-    <>
-      <CharacterEditor />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <PerspectiveDecoration />
+            <CharacterEditor />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/PerspectiveDecoration/PerspectiveDecoration.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './PerspectiveDecoration.module.css';
+
+const PerspectiveDecoration = () => {
+    return <div className={styles.perspectiveDecoration}></div>;
+};
+
+export default PerspectiveDecoration;
